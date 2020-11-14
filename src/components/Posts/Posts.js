81ba404ffdd9b42e9102c9b6f19696a2ac8e4042
sarah rose <xsarahrosemeow@gmail.com>
2020-11-14 13:28:34 -0500

    all mvp finished

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import Post from './Post';
-import './Posts.css';
+import React from "react";
+import Post from "./Post";
+import "./Posts.css";
 
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
+  console.log(props);
 
   return (
-    <div className='posts-container-wrapper'>
+    <div className="posts-container-wrapper">
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
+      {posts.map((post, index) => {
+        return <Post key={index} post={post} likePost={likePost} />;
+      })}
     </div>
   );
 };
